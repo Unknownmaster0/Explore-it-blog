c56427b1608f0d3ce0db729c53c2d2b49c19b543
Unknownmaster0 <sagar192002singh@gmail.com>
2025-04-29 19:33:31 +0530

    added the post api end points and its corresponding urls

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -8,4 +8,13 @@ urlpatterns = [
     path('user/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('user/register/', api_views.RegisterView.as_view(), name='register'),
     path('user/profile/<user_id>/', api_views.ProfileView.as_view(), name='profile'),
+
+    #post urls
+    path('post/category/list/', api_views.CategoryListApiView.as_view(), name='category-list'),
+    path('post/category/posts/<category_slug>/', api_views.PostCategoryListApiView.as_view(), name='post-category-list'),
+    path('post/list/', api_views.PostListApiView.as_view(), name='post-list'),
+    path('post/detail/<slug>/', api_views.PostDetailApiView.as_view(), name='post-detail'),
+    path('post/like-post/', api_views.LikePostApiView.as_view(), name='like-post'),
+    path('post/bookmark-post/', api_views.BookmarkPostApiView.as_view(), name='bookmark-post'),
+    path('post/comment/', api_views.PostCommentApiView.as_view(), name='post-comment'),
 ]
--- backend/api/views.py
+++ backend/api/views.py
@@ -29,6 +29,7 @@ import random
 from api import serializers as api_serializer
 from api import models as api_models
 
+# user releated views
 class MyTokenObtainPairView(TokenObtainPairView):
     # Here, it specifies the serializer class to be used with this view.
     serializer_class = api_serializer.MyTokenObtainPairSerializer
@@ -51,4 +52,126 @@ class ProfileView(generics.RetrieveUpdateAPIView):
         user = api_models.User.objects.get(id=user_id)
         profile = api_models.Profile.objects.get(user=user)
         return profile
+    
+# post views
+
+class CategoryListApiView(generics.ListAPIView):
+    serializer_class = api_serializer.CategorySerializer
+    permission_classes = [AllowAny]
+
+    def get_queryset(self):
+        return api_models.Category.objects.all()
+    
+class PostCategoryListApiView(generics.ListAPIView):
+    serializer_class = api_serializer.PostSerializer
+    permission_classes = [AllowAny]
+
+    def get_queryset(self):
+        category_slug = self.kwargs['category_slug']
+        category = api_models.Category.objects.get(slug=category_slug)
+        return api_models.Post.objects.filter(category=category, status='Active').order_by('-created_at')
+    
+class PostListApiView(generics.ListAPIView):
+    serializer_class = api_serializer.PostSerializer
+    permission_classes = [AllowAny]
+
+    def get_queryset(self):
+        return api_models.Post.objects.all().order_by('-created_at')
+    
+class PostDetailApiView(generics.RetrieveAPIView):
+    serializer_class = api_serializer.PostSerializer
+    permission_classes = [AllowAny]
+
+    def get_queryset(self):
+        slug = self.kwargs['slug']
+        post = api_models.Post.objects.get(slug=slug, status='Active')
+        post.view += 1
+        post.save()
+        return post;
+
+class LikePostApiView(APIView):
+    @swagger_auto_schema(
+        request_body=openapi.Schema(
+            type=openapi.TYPE_OBJECT,
+            properties={
+                'user_id': openapi.Schema(type=openapi.TYPE_INTEGER),
+                'post_id': openapi.Schema(type=openapi.TYPE_INTEGER),
+            },
+        ),
+    )
+
+    def post(self, request):
+        user_id = request.data.get('user_id')
+        post_id = request.data.get('post_id')
+
+        user = api_models.User.objects.get(id=user_id)
+        post = api_models.Post.objects.get(id=post_id)
+
+        if user in post.likes.all():
+            post.likes.remove(user)
+            post.save()
+            return Response({'message': 'Post unliked successfully'}, status=status.HTTP_200_OK)
+        else:
+            post.likes.add(user)
+            post.save()
+            api_models.Notification.objects.create(user=post.user, post=post, type='Like')
+            return Response({'message': 'Post liked successfully'}, status=status.HTTP_201_CREATED)
+        
+class PostCommentApiView(APIView):
+    @swagger_auto_schema(
+        request_body=openapi.Schema(
+            type=openapi.TYPE_OBJECT,
+            properties={
+                'post_id': openapi.Schema(type=openapi.TYPE_INTEGER),
+                'name' : openapi.Schema(type=openapi.TYPE_STRING),
+                'email' : openapi.Schema(type=openapi.TYPE_STRING),
+                'comment' : openapi.Schema(type=openapi.TYPE_STRING),
+            },
+        ),
+    )
+
+    def post(self, request):
+        post_id = request.data.get('post_id')
+        name = request.data.get('name')
+        email = request.data.get('email')
+        comment = request.data.get('comment')
+
+        post = api_models.Post.objects.get(id=post_id)
+
+        api_models.Comment.objects.create(post=post, name=name, email=email, comment=comment)
+
+        api_models.Notification.objects.create(user=post.user, post=post, type='Comment')
+
+        return Response({'message': 'Comment added successfully'}, status=status.HTTP_201_CREATED)
+    
+class BookmarkPostApiView(APIView):
+    @swagger_auto_schema(
+        request_body=openapi.Schema(
+            type=openapi.TYPE_OBJECT,
+            properties={
+                'user_id': openapi.Schema(type=openapi.TYPE_INTEGER),
+                'post_id': openapi.Schema(type=openapi.TYPE_STRING),
+            },
+        ),
+    )
+
+    def post(self, request):
+        user_id = request.data.get('user_id')
+        post_id = request.data.get('post_id')
+
+        user = api_models.User.objects.get(id=user_id)
+        post = api_models.Post.objects.get(id=post_id)
+
+        bookmark = api_models.Bookmark.objects.filter(user=user, post=post).first()
+
+        if bookmark:
+            bookmark.delete()
+            return Response({'message': 'Post removed from bookmarks'}, status=status.HTTP_200_OK)
+        else:
+            api_models.Bookmark.objects.create(user=user, post=post)
+
+        api_models.Notification.objects.create(user=post.user, post=post, type='Bookmark')
+
+        return Response({'message': 'Post bookmarked successfully'}, status=status.HTTP_201_CREATED)
+    
     
