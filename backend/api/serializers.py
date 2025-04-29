708097ad8df00c7daee49bcaad0c1485661d7127
Unknownmaster0 <sagar192002singh@gmail.com>
2025-04-29 18:35:43 +0530

    updated the serilizers and created the views and urls for user part

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -6,6 +6,58 @@ from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 
 from api import models as api_models
 
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    # Define a custom method to get the token for a user
+    def get_token(cls, user):
+        # Call the parent class's get_token method
+        token = super().get_token(user)
+
+        # Add custom claims to the token
+        token['full_name'] = user.full_name
+        token['email'] = user.email
+        try:
+            token['vendor_id'] = user.vendor.id
+        except:
+            token['vendor_id'] = 0
+
+        # Return the token with custom claims
+        return token
+
+class RegisterSerializer(serializers.ModelSerializer):
+    # Define fields for the serializer, including password and password2
+    password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
+    password2 = serializers.CharField(write_only=True, required=True)
+
+    class Meta:
+        # Specify the model that this serializer is associated with
+        model = api_models.User
+        # Define the fields from the model that should be included in the serializer
+        fields = ('full_name', 'email',  'password', 'password2')
+
+    def validate(self, attrs):
+        # Define a validation method to check if the passwords match
+        if attrs['password'] != attrs['password2']:
+            # Raise a validation error if the passwords don't match
+            raise serializers.ValidationError({"password": "Password fields didn't match."})
+
+        # Return the validated attributes
+        return attrs
+
+    def create(self, validated_data):
+        # Define a method to create a new user based on validated data
+        user = api_models.User.objects.create(
+            full_name=validated_data['full_name'],
+            email=validated_data['email'],
+        )
+        # Set the user's password based on the validated data
+        user.set_password(validated_data['password'])
+        user.save()
+
+        # Return the created user
+        return user
+
+
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = api_models.User
