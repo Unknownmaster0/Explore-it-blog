5a22b5673eef272470de3fb6aa5c915932fa50bf
Unknownmaster0 <sagar192002singh@gmail.com>
2025-04-30 11:28:31 +0530

    updated the swagger url

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -13,13 +13,14 @@ schema_view = get_schema_view(
         default_version='v1',
     ),
     public=True,
-    permission_classes=(permissions.AllowAny,)
+    permission_classes=(permissions.AllowAny,),
+    url='http://localhost:8000/api/v1/',
 )
 
 urlpatterns = [
     path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('admin/', admin.site.urls),
-    path('api/', include('api.urls')),
+    path('api/v1/', include('api.urls')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
