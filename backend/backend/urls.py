ba1c0f4c74a6c819d795ce23b8e2cf6a81c74838
Unknownmaster0 <sagar192002singh@gmail.com>
2025-05-04 19:31:59 +0530

    updated url.py of the backend

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -9,18 +9,22 @@ from drf_yasg import openapi
 
 schema_view = get_schema_view(
     openapi.Info(
-        title="API",
-        default_version='v1',
+        title="Blog Backend APIs",
+        default_version="v1",
     ),
     public=True,
-    permission_classes=(permissions.AllowAny,),
-    url='http://localhost:8000/api/v1/',
+    permission_classes = (permissions.AllowAny, ),
+    url='http://localhost:8000/api/v1/',  # Add this line
+    patterns=[
+        path('api/v1/', include('api.urls')),
+    ],
 )
 
 urlpatterns = [
-    path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('admin/', admin.site.urls),
     path('api/v1/', include('api.urls')),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
